82abf292af60ee076facfb42b09d4b0390942077
Struan Donald <struan@exo.org.uk>
2016-01-08 17:48:18 +0000

    include all name options in the search index
    make sure we are putting all the name options in to the search index so
    that they will match when searching for them
    
    Most of these aren't accessible through the UI but better to start of
    indexing them in case they are included in a popolo import

--- candidates/search_indexes.py
+++ candidates/search_indexes.py
@@ -4,10 +4,13 @@ from popolo.models import Person
 
 
 class PersonIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    # FIXME: this doesn't seem to work for partial names despite what
+    # docs say
+    text = indexes.EdgeNgramField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+    family_name = indexes.CharField(model_attr='family_name')
+    given_name = indexes.CharField(model_attr='given_name')
+    additional_name = indexes.CharField(model_attr='additional_name')
 
     def get_model(self):
         return Person
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
